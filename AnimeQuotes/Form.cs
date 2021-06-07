ac4a6e05c4ba3bd286a238d6ecc11a2479fa1724
Waldemar <vladimir-vladimir2003@mail.ru>
2021-06-07 22:48:59 +0300

    Add project files.

---
+++ AnimeQuotes/Form.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace AnimeQuotes
+{
+    public partial class Form
+    {
+        [JsonProperty("anime")]
+        public string Anime { get; set; }
+    
+        [JsonProperty("character")]
+        public string Character { get; set; }
+    
+        [JsonProperty("quote")]
+        public string Quote { get; set; }
+    }
+}
---
+++ AnimeQuotes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace AnimeQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = $"https://animechan.vercel.app/api/random";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                //Console.WriteLine(result);
+                var animeQuote = JsonConvert.DeserializeObject<Form>(result);
+                Console.WriteLine($"Anime: {animeQuote.Anime}");
+                Console.WriteLine($"Character: {animeQuote.Character}");
+                Console.WriteLine($"Quote: {animeQuote.Quote}");
+            }
+
+        }
+    }
+}
